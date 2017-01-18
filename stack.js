99caa6f604fa15de627d827526a658a49e27c7b5
keegan ridgley <keegan.cabin@gmail.com>
2017-01-18 10:25:35 -0700

    remade part of stack functionality using js

--- stack.js
+++ stack.js
@@ -1,5 +1,33 @@
+"use strict";
+
 function Stack(){
+this.myStack = new Array();
+var that = this;
+that.empty = function(){
+  if (that.myStack.length === 0) {
+    return true
+    }else {
+    return false
+  }
+  }
+  that.peek = function(){
+    var len = that.myStack.length;
+    if (len !==0) {
+      return that.myStack[0]
+    }else {
+      return null
+    }
+  }
+that.push = function(data){
+  that.myStack.push(data)
+  return data;
+  };
+that.pop = function(){
 
 }
+
+}
+
+
 
 module.exports = Stack;
